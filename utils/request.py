a95f0d2a6a2335e4e3370761faee4a5bb839aff0
zakky <kycraft1164@gmail.com>
2024-12-13 17:54:36 +0700

    move formOrJson dependency to core.dependencies

--- utils/request.py
+++ utils/request.py
@@ -1,21 +1,7 @@
 from typing import TypeVar, Type, Literal
-from fastapi import Request, Depends
 from pydantic import BaseModel
-from core.exceptions.http import CustomHttpException
 
 _TModel = TypeVar("_TModel", bound=BaseModel)
-
-def formOrJson(model: Type[_TModel]) -> _TModel:
-    async def formOrJsonInner(request: Request) -> _TModel:
-        type_ = request.headers["Content-Type"].split(";", 1)[0]
-        if type_ == "application/json":
-            data = await request.json()
-        elif type_ == "multipart/form-data":
-            data = await request.form()
-        else:
-            raise CustomHttpException(status_code=415, message="Unsupported Media Type")
-        return model.model_validate(data)
-    return Depends(formOrJsonInner)
 
 def generateFormOrJsonOpenapiBody(
     model: Type[_TModel],
